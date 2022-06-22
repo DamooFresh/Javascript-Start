93e91246519c3b461eae01871bfc5e152a87d561
Oluwadare Oketokun <oketokund@gmail.com>
2022-06-22 12:40:01 +0100

    initial commit

---
+++ java.js
@@ -0,0 +1,4 @@
+        namee = prompt("what is your name?")
+        height = prompt("What is your height?")
+        country = prompt("What is the name of your country")
+        alert("Your name is " + namee + ", your height is " + height + " and you are from " + country)
